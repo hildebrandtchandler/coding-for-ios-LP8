4aa45e13f0a69a810bf8df0a5700e3724335a2a1
hildebrandtchandler <hildebrandtchandler@gmail.com>
2022-12-15 04:18:11 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -61,3 +61,15 @@ self.addEventListener('fetch', (event) => {
         })
     );
   });
+
+  async function registerPeriodicNewsCheck() {
+    const registration = await navigator.serviceWorker.ready;
+    try {
+      await registration.periodicSync.register('get-latest-news', {
+        minInterval: 24 * 60 * 60 * 1000,
+      });
+    } catch {
+      console.log('Periodic Sync could not be registered!');
+    }
+  }
+  
